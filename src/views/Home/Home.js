ab7833ea7cd38119c11fd181d6812cdeec844379
Phobial <harshalpatilcsn2020@gmail.com>
2024-07-14 11:50:32 +0530

    social-media links added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
 
+import React from 'react'
+import '../../views/Home/Home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="media-container">
+        <div className='social-media  py-3'>
+          <i className="fa-brands fa-facebook-f"><a href=''></a> </i>
+          <i className="fa-brands fa-twitter"><a href=''></a></i>
+          <i className="fa-brands fa-instagram"><a href=''></a></i>
+          <i className="fa-brands fa-linkedin-in"><a href=''></a></i>
+        </div>
+      </div>
+    </>
   )
 }
 
