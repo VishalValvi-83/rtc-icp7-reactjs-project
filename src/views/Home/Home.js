3288f2792e7ebe2c09ee0f4dd9948731fdfe7670
Phobial <harshalpatilcsn2020@gmail.com>
2024-07-14 18:14:04 +0530

    header and bavbar changed

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -15,17 +15,31 @@ const Home = () => {
           <i className="fa-brands fa-linkedin-in"><a href=''></a></i>
         </div>
       </div>
-      <div className='header my-4'>
+      {/* <div className='header my-4'>
         <ul className='d-flex list-inline align-items-center mx-auto'>
-          <li><img src={logo} className='logo' /></li>
-          <li className=''><Link to="" className='nevigation-menu'>Home</Link></li>
-          <li><Link to="" className='nevigation-menu'>About</Link></li>
-          <li><Link to="" className='nevigation-menu'>Product</Link></li>
-          <li><Link to="" className='nevigation-menu'>Contact</Link></li>
-          <li><Link to="" className='nevigation-menu'>signup</Link></li>
-          <li><button className='btn btn-warning login-btn nevigation-menu'>Login</button></li>
+      
         </ul>
-      </div>
+      </div> */}
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid list-inline ">
+          <a class="navbar-brand" href=""><li><img src={logo} className='logo' /></li></a>
+          
+          <div class="collapse navbar-collapse " id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+              <li className=''><Link to="" className='nevigation-menu'>Home</Link></li>
+              <li><Link to="" className='nevigation-menu'>About</Link></li>
+              <li><Link to="" className='nevigation-menu'>Product</Link></li>
+              <li><Link to="" className='nevigation-menu'>Contact</Link></li>
+              <li><Link to="" className='nevigation-menu'>signup</Link></li>
+              <li><button className='btn btn-warning login-btn nevigation-menu'>Login</button></li>
+            </ul>
+          </div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+        </div>
+      </nav>
       <div className='banner-section'>
         <div className='banner-text'>
           <p className='our'>Our</p>
@@ -33,6 +47,10 @@ const Home = () => {
           <p className='product'>Products</p>
           <button className='btn btn-warning product-btn'><a href=''>see more Products</a> </button>
         </div>
+      </div>
+      <div className='choose-section my-5'>
+        <p className='choose-us'>Why <span className='black-text'>Choose Us</span></p>
+        <p className='text'>Fastest repair service with best price!</p>
       </div>
     </>
   )
