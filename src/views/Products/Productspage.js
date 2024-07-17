9fe5ce646f96c7a0c4547ceba0d6fab822d6293d
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-17 16:28:18 +0530

    added the code of productspage

--- src/views/Products/Productspage.js
+++ src/views/Products/Productspage.js
@@ -1,8 +1,31 @@
 import React from 'react'
-import {Products}  from '../../components/Products/Products'
+import './productspage.css'
 function Productspage() {
-    return (
-        <div><Products /></div>
+    return (<>
+        <div className="brand_color">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="titlepage">
+                            <h2>our product</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="products m-5">
+            <div className="title text-center">
+                <span className="mt-3 fs-4">We package the products with best services to make you a <span className='customer'>happy customer </span>.</span>
+            </div>
+        </div>
+        <div className="product-bg-white">
+            <div className="container">
+                <div className="row">
+                </div>
+            </div>
+        </div>
+    </>
+
     )
 }
 
