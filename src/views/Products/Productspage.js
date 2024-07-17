b139cbad8f3dd100aadceef24b5a78f0b9274cf9
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-17 15:49:24 +0530

    created files of products page

---
+++ src/views/Products/Productspage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Products}  from '../../components/Products/Products'
+function Productspage() {
+    return (
+        <div><Products /></div>
+    )
+}
+
+export default Productspage
