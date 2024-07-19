d2383a32acd0af432ae8fcd7695e1f7d376d972e
sanket7472 <sankettamnar2003@gmail.com>
2024-07-19 08:28:55 +0530

    about page started

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div><div class="about">
+   <div class="container">
+      <div class="row">
+         <dir class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="about_box">
+               <figure><img src="images/pc_layout.png"/></figure>
+            </div>
+         </dir>
+          <dir class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="about_box">
+               <h3>Who is Lighten</h3>
+               <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.</p>
+               <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+         </dir> 
+      </div>
+   </div>
+</div></div>
+    
+    
+    </>
+  
   )
 }
 
