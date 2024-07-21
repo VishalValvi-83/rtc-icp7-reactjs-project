353f7acd181f40fbfabd2be169c293993afbba30
sanket7472 <sankettamnar2003@gmail.com>
2024-07-21 19:14:37 +0530

    about page designed

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
               <dir className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
                 <div >
                   <h3 className="about_box">Who is Lighten</h3>
-                  <p >It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.</p>
+                  <p > It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.</p>
                   <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
               </dir>
